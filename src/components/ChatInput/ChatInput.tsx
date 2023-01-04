52abf6045f15b9ff48404ead3386243c25d04b09
Can Berk Ocalir <can.ocalir@gmail.com>
2023-01-04 18:57:31 +0300

    Created chat input component

---
+++ src/components/ChatInput/ChatInput.tsx
@@ -0,0 +1,11 @@
+type Props = {
+    channelId: string | null
+}
+
+const ChatInput = ({channelId}:Props) => {
+  return (
+    <input type="text" />
+  )
+}
+
+export default ChatInput
--- src/views/Chat/Chat.tsx
+++ src/views/Chat/Chat.tsx
@@ -1,44 +1,80 @@
-import { InfoOutlined, StarBorderOutlined } from "@material-ui/icons"
-import styled from "styled-components"
+import { InfoOutlined, StarBorderOutlined } from "@material-ui/icons";
+import styled from "styled-components";
+import { useAppSelector } from "../../app/hooks";
+import ChatInput from "../../components/ChatInput/ChatInput";
 
 const Chat = () => {
+  const {roomId} = useAppSelector(state => state.room)
   return (
     <ChatContainer>
-        <ChatHeader>
+      <>
+      <ChatHeader>
         <ChatHeaderLeft>
-            <h4><strong>#Room name</strong></h4>
-            <StarBorderOutlined/>
-          </ChatHeaderLeft>
-          <ChatHeaderRight>
-            <p>
-              <InfoOutlined/>
-              Details
-            </p>
-          </ChatHeaderRight>
-        </ChatHeader>
+          <h4>
+            <strong>#Room name</strong>
+          </h4>
+          <StarBorderOutlined />
+        </ChatHeaderLeft>
+        <ChatHeaderRight>
+          <p>
+            <InfoOutlined />
+            Details
+          </p>
+        </ChatHeaderRight>
+      </ChatHeader>
+      </>
+      <ChatMessages>
+    
+      </ChatMessages>
+      <ChatInput
+      channelId={roomId}
+      />
     </ChatContainer>
-  )
-}
+  );
+};
 
-export default Chat
+export default Chat;
+
+const ChatMessages = styled.div`
+  
+`
 
 const ChatContainer = styled.div`
-    flex: 0.7;
-    flex-grow: 1;
-    overflow-y: scroll;
-    margin: 4rem 0 0 1rem;
-    margin-top: 5rem;
-`
+  flex: 0.7;
+  flex-grow: 1;
+  overflow-y: scroll;
+  margin: 4rem 0 0 1rem;
+  margin-top: 5rem;
+`;
 
 const ChatHeader = styled.div`
   display: flex;
   justify-content: space-between;
   padding: 1.2rem;
   border-bottom: 0.1rem solid #e9e9e9;
-`
+`;
 
-const ChatHeaderRight = styled.div``
+const ChatHeaderRight = styled.div`
+  p {
+    display: flex;
+    align-items: center;
+    font-size: 0.8rem;
+  }
+  .MuiSvgIcon-root {
+    font-size: 1.4rem;
+    margin-right: 0.2rem;
+  }
+`;
 
 const ChatHeaderLeft = styled.div`
-
-`
+  display: flex;
+  h4 {
+    display: flex;
+    align-items: center;
+    text-transform: lowercase;
+    margin-right: 0.4rem;
+  }
+  .MuiSvgIcon-root {
+    font-size: 1.4rem;
+  }
+`;
